0f7627db1f36a938b1648eec942fb4dbf9f731f9
Henrique Gatto <34990148-henriquemonte12@users.noreply.replit.com>
2024-09-05 01:08:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let anoAtual = 2024;
+const nomeCompelto = "Henrique Gatto"
+let idade = 17
+const cursoFaculdade = "direito"
+let cidadeAtual = "Paris"
+                                                            
+
+console.log(anoAtual);
+
+anoAtual = 2025;
+
+console.log(anoAtual);
